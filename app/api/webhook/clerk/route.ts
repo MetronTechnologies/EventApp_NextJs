b75b6b3dac598ce72660e4b6bb30fa205724b28e
JEkanem <jeremiahekanem2@gmail.com>
2024-05-26 21:01:43 +0100

    correction for first deployment

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -85,10 +85,10 @@ export async function POST(req: Request) {
         const {id, image_url, first_name, last_name, username } = evt.data
 
         const user = {
-            firstName: first_name,
-            lastName: last_name,
+            firstName: first_name!,
+            lastName: last_name!,
             username: username!,
-            photo: image_url,
+            photo: image_url!,
         }
 
         const updatedUser = await updateUser(id, user)
