bbdec771cdb5af8978c892a0ad47cff5358b3060
JEkanem <jeremiahekanem2@gmail.com>
2024-05-29 18:09:19 +0100

    changes 4

--- middleware.ts
+++ middleware.ts
@@ -2,20 +2,28 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware({
     publicRoutes: [
-        '/',
-        '/events/:id',
-        '/api/webhook/clerk',
-        '/api/webhook/stripe',
-        '/api/uploadthing'
+        "/",
+        "/events/:id",
+        "/api/webhook/clerk",
+        "/api/webhook/stripe",
+        "/api/uploadthing"
     ],
     ignoredRoutes: [
-        '/api/webhook/clerk',
-        '/api/webhook/stripe',
-        '/api/uploadthing'
+        "/api/webhook/clerk",
+        "/api/webhook/stripe",
+        "/api/uploadthing"
     ]
 });
 
 export const config = {
     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+
+// export default clerkMiddleware({
+//     publicRoutes: ["/", "/api/webhook/clerk", "/admin", "/api/webhooks/user"],
+//     ignoredRoutes: ["/api/webhook/clerk"],
+// });
+
+
 
