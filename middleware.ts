fc51689cd77bc9b962e5e3a3c5c5dbf05c8e132d
JEkanem <jeremiahekanem2@gmail.com>
2024-05-29 20:35:01 +0100

    webhook

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,28 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+//
+// export default clerkMiddleware();
+// export const config = {
+//     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+//     publicRoutes: [
+//         '/',
+//         '/events/:id',
+//         '/api/webhook/clerk',
+//         '/api/webhook/stripe',
+//         '/api/uploadthing'
+//     ],
+//     ignoredRoutes: [
+//         '/api/webhook/clerk',
+//         '/api/webhook/stripe',
+//         '/api/uploadthing'
+//     ]
+// };
 
-export default clerkMiddleware();
-export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+
+
+
+import {authMiddleware} from "@clerk/nextjs/server";
+
+export default authMiddleware({
     publicRoutes: [
         '/',
         '/events/:id',
@@ -15,6 +35,10 @@ export const config = {
         '/api/webhook/stripe',
         '/api/uploadthing'
     ]
+});
+
+export const config = {
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
 
 
