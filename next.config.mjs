0c818b832d19d2654b09912d859eb87d2bbc2d3d
JEkanem <jeremiahekanem2@gmail.com>
2024-05-29 18:28:59 +0100

    changes 6

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    typescript: {
+        ignoreBuildErrors: true
+    },
+    images: {
+        domains: ['utfs.io'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+                port: ''
+            }
+        ]
+    }
+};
 
 export default nextConfig;
