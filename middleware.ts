f50a830fd9c8ed06348016ccf3560df3cecf7aa3
JEkanem <jeremiahekanem2@gmail.com>
2024-05-29 18:41:56 +0100

    changes 7

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,43 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+//
+// export default clerkMiddleware({
+//     publicRoutes: ["/", "/events/:id", "/api/webhook/clerk", "/api/webhook/stripe", "/api/uploadthing"
+//     ],
+//     ignoredRoutes: [
+//         "/api/webhook/clerk",
+//         "/api/webhook/stripe",
+//         "/api/uploadthing"
+//     ]
+// });
+//
+// export const config = {
+//     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+//
+//
 
-export default clerkMiddleware({
-    publicRoutes: ["/", "/events/:id", "/api/webhook/clerk", "/api/webhook/stripe", "/api/uploadthing"
+
+
+import {authMiddleware} from "@clerk/nextjs/server";
+
+export default authMiddleware({
+    publicRoutes: [
+        '/',
+        '/events/:id',
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        '/api/uploadthing'
     ],
     ignoredRoutes: [
-        "/api/webhook/clerk",
-        "/api/webhook/stripe",
-        "/api/uploadthing"
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        '/api/uploadthing'
     ]
 });
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
-
-
-// export default clerkMiddleware({
-//     publicRoutes: ["/", "/api/webhook/clerk", "/admin", "/api/webhooks/user"],
-//     ignoredRoutes: ["/api/webhook/clerk"],
-// });
 
 
 
