07c77c060feeee116d5d43cf0713eb16cd23fe9c
JEkanem <jeremiahekanem2@gmail.com>
2024-06-01 12:10:58 +0100

    webhook config

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,56 +1,63 @@
-import {Webhook, WebhookRequiredHeaders} from 'svix'
+import { Webhook } from 'svix';
 import { headers } from 'next/headers';
 import {clerkClient, WebhookEvent} from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
-import {createUser, deleteUser, updateUser} from "@/lib/actions/user.action";
-import {CreateUserParams, UpdateUserParams} from "@/types";
-import {IncomingHttpHeaders} from "http";
+import {CreateUserParams} from "@/types";
+import {createUser, deleteUser} from "@/lib/actions/user.action";
 
-type EventType =
-    | "user.created"
-    | "user.updated"
-    | "user.deleted";
+export async function POST(req: Request) {
 
-type Event = {
-    data: Record<string, string | number | Record<string, string>[]>;
-    object: "event";
-    type: EventType;
-};
+    // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
+    const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+    console.log("Webhook secret ---> " + WEBHOOK_SECRET);
 
-export const POST = async (request: Request) => {
-    const payload = await request.json();
-    const header = headers();
-
-    const heads = {
-        "svix-id": header.get("svix-id"),
-        "svix-timestamp": header.get("svix-timestamp"),
-        "svix-signature": header.get("svix-signature"),
-    };
-
-    // Activate Webhook in the Clerk Dashboard.
-    // After adding the endpoint, you'll see the secret on the right side.
-    console.log("Env ---> " + process.env.NEXT_CLERK_WEBHOOK_SECRET);
-    const wh = new Webhook(process.env.NEXT_CLERK_WEBHOOK_SECRET || "");
-
-    let evnt: Event | null = null;
-
-    try {
-        evnt = wh.verify(
-            JSON.stringify(payload),
-            heads as IncomingHttpHeaders & WebhookRequiredHeaders
-        ) as Event;
-    } catch (err) {
-        return NextResponse.json(
-            { message: err },
-            // { status: 400 }
-        );
+    if (!WEBHOOK_SECRET) {
+        throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
     }
 
-    const eventType: EventType = evnt?.type!;
+    // Get the headers
+    const headerPayload = headers();
+    const svix_id = headerPayload.get("svix-id");
+    const svix_timestamp = headerPayload.get("svix-timestamp");
+    const svix_signature = headerPayload.get("svix-signature");
+
+    // If there are no headers, error out
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+        return new Response('Error occured -- no svix headers', {
+            status: 400
+        })
+    }
+
+    // Get the body
+    const payload = await req.json()
+    const body = JSON.stringify(payload);
+
+    // Create a new Svix instance with your secret.
+    const wh = new Webhook(WEBHOOK_SECRET);
+
+    let evt: WebhookEvent
+
+    // Verify the payload with the headers
+    try {
+        evt = wh.verify(body, {
+            "svix-id": svix_id,
+            "svix-timestamp": svix_timestamp,
+            "svix-signature": svix_signature,
+        }) as WebhookEvent
+    } catch (err) {
+        console.error('Error verifying webhook:', err);
+        return new Response('Error occured', {
+            status: 400
+        })
+    }
+
+    // Get the ID and type
+    const { id } = evt.data;
+    const eventType = evt.type;
 
     if(eventType === 'user.created') {
         console.log("Action is created");
-        const { id, email_addresses, image_url, first_name, last_name, username } = evnt.data;
+        const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
         const user= <CreateUserParams>{
             clerkId: id,
@@ -92,7 +99,7 @@ export const POST = async (request: Request) => {
     // }
 
     if (eventType === 'user.deleted') {
-        const { id } = evnt?.data
+        const { id } = evt?.data
 
         const deletedUser = await deleteUser(id!)
 
